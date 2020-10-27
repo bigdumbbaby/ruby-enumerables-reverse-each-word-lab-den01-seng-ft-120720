0d6bad9a18730f8c87ff6d9d0edcad8b8a4e4cc4
Colton O'Connor <colton.shawn.oconnor@gmail.com>
2020-10-27 04:06:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
   array = string.split(" ")
-  new_array = array.each { |element| element.reverse() }
-  return array.join(" ")
+  result = array.each { |element| element.reverse() }
+  return result.join(" ")
 end
